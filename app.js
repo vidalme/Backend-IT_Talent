6d1b515a29093bdc5973a57cf0128bb18ed87cbd
Andre Vidal <121238740+vidalme@users.noreply.github.com>
2024-06-18 21:56:29 -0300

    trocando pelo nome completo da env var APP_PORT

--- app.js
+++ app.js
@@ -60,5 +60,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(process.env.APP_PORT, () => {
-    // console.log(`O servidor está executando na porta ${APP_PORT}`);
+    console.log(`O servidor está executando na porta ${process.env.APP_PORT}`);
 });
